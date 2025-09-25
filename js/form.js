c6568fa795818d0b5a4acf3cfcf8c6f7e0d1198f
GPDEVOPS <gchalo17@gmail.com>
2025-09-25 16:44:24 -0500

    modify js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,11 @@
+//Verificar los valores de entrada
 function guardar() {
     let nombres = document.forms["formDatos"]["nombres"].value;
-    alert("Los nombres ingresados son: " + nombres)
+    //alert("Los nombres ingresados son: " + nombres)
+    let apellidos = document.forms["formDatos"]["apellidos"].value;
+    let numeroTelefonico = document.forms["formDatos"]["numeroTelefonico"].value;
+    let correo = document.forms["formDatos"]["correo"].value;
+    let pais = document.forms["formDatos"]["pais"].value;
+    let ciudad = document.forms["formDatos"]["ciudad"].value;
+    alert("Datos guardados con exito" + nombres)
 }
