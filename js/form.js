ccad643214ff8c9321668263986107bc22d1a988
GPDEVOPS <gchalo17@gmail.com>
2025-09-25 17:21:03 -0500

    form completed htm and js

--- js/form.js
+++ js/form.js
@@ -3,9 +3,13 @@ function guardar() {
     let nombres = document.forms["formDatos"]["nombres"].value;
     //alert("Los nombres ingresados son: " + nombres)
     let apellidos = document.forms["formDatos"]["apellidos"].value;
-    let numeroTelefonico = document.forms["formDatos"]["numeroTelefonico"].value;
+    //alert("Los apellidos ingresados son: " + apellidos)
+    let numeroTelefono = document.forms["formDatos"]["numeroTelefono"].value;
+    //alert("El numero telefonico ingresado es: " + numeroTelefono)
     let correo = document.forms["formDatos"]["correo"].value;
+    //alert("El correo ingresado es: " + correo)
     let pais = document.forms["formDatos"]["pais"].value;
+    //alert("El pais ingresado es: " + pais)
     let ciudad = document.forms["formDatos"]["ciudad"].value;
-    alert("Datos guardados con exito" + nombres)
+    alert("Datos ingresados con exito: ")
 }
